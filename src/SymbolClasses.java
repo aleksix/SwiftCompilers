0787cab1f47efbaf486e13de0e9dd477ad32e055
Alex Gospodchikov <aleks-say@mail.ru>
2018-09-20 11:23:43 +0300

    Added more infrastructure needed for parsing.

--- src/SymbolClasses.java
+++ src/SymbolClasses.java
@@ -3,4 +3,25 @@ public class SymbolClasses {
     public static boolean isWhitespace(int character) {
         return false;
     }
+
+    public static boolean isIdentifierSymbol(int character) {
+        return false;
+    }
+
+    public static boolean isOperatorSymbol(int character) {
+        return false;
+    }
+
+    // Not sure if the following functions are needed, just copied the official docs.
+    public static boolean isIdentifierHead(int chracter) {
+        return false;
+    }
+
+    public static boolean isValidTextItem(int character) {
+        return false;
+    }
+
+    public static boolean isOperatorHead(int character) {
+        return false;
+    }
 }
