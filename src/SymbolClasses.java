199a1608a009e54da3504cdb3cc1c2ab2429796f
Riceshark <enotpetya@gmail.com>
2018-09-23 20:01:11 +0300

    Update SymbolClasses.java

--- src/SymbolClasses.java
+++ src/SymbolClasses.java
@@ -202,6 +202,16 @@ public class SymbolClasses {
             case '~':
             case '?':
             case '.':
+            case '@':
+            case '{':
+            case '}':
+            case '(':
+            case ')':
+            case '[':
+            case ']':
+            case ',':
+            case ':':
+            case ';':
             case 0x3030:
             case 0x2016:
             case 0x2017:
